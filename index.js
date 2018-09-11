ee59d192cef929c9d189c0e756c82d59696a246c
midnight <sofilogvin@gmail.com>
2018-09-11 23:26:44 +0300

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const http = require("http");
+import express from "express";
+import { createServer } from "http";
 
 //  Express app setup
 const app = express();
 
-const server = http.createServer(app);
+const server = createServer(app);
 server.listen(3000);
 server.on('listening', () => {
     console.log('Server is listening on port: 3000');
